a9f83ca3b03bb7766a1b647e869c94468765d257
Alex <carriealex2001@gmail.com>
2023-07-21 19:23:56 -0700

    create object for nearby search

--- src/place_search.py
+++ src/place_search.py
@@ -1,26 +1,36 @@
 from arcgis.gis import GIS
 from arcgis.geocoding import geocode
 import json
-
-def search_places(location, category, max_locations=25):
-    gis = GIS()
-    geocoded_features = geocode(
-        address=None,
-        location=location,
-        category=category,
-        out_fields="Place_addr, PlaceName",
-        max_locations=max_locations,
-        as_featureset=True,
-    )
-    return geocoded_features
+class NearbySearch:
+    def __init__(self, location: tuple(float, float)):
+        self.location = location
+        self.gis=GIS()
+        
+    def search_places(self, category, search_radius=25, max_locations=25):
+        geocoded_features = geocode(
+            address=None,
+            location=self.location,
+            category=category,
+            out_fields="Place_addr, PlaceName",
+            max_locations=max_locations,
+            distance=search_radius,
+            as_featureset=True,
+        )
+        return geocoded_features
+    
+    
 
 
 if __name__ == '__main__':
     location=[-118.71511, 34.09042]
     category="Coffee shop"
-    # result = json.loads(search_places(location, category)).to_json()
-    result = search_places(location, category)
+    obj = NearbySearch(location)
+    
+    result = obj.search_places(location, category)
+    # result = search_places(location, category).to_dict()
     # print(result.to_json())
-    print(json.load(str(result)))
-    print(result)
-    print(0)
+    # As a DF:
+    # geocoded_df = result.sdf
+    
+    # print(json.load(str(result)))
+    print(type(result))
